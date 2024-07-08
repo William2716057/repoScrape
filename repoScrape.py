08ba08ce78400f0b615df97ca5a8ab0a1e89435e
William <willspri@gmail.com>
2024-07-08 19:14:15 +1200

    printing repo list

--- repoScrape.py
+++ repoScrape.py
@@ -1,11 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-
 repo = input("enter github profile name: ")
 
-URL = "https://github.com/" + repo + "?tab=repositories"
+#URL = "https://github.com/" + repo + "?tab=repositories"
+
+URL = f"https://github.com/{repo}?tab=repositories"
 
 print(URL)
 
-path_to_scrape = requests.get(URL)
-print(path_to_scrape)
+response = requests.get(URL)
+print(response)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    repos = soup.find_all('a', itemprop='name codeRepository')
+    
+    for repo in repos:
+        print(repo.text.strip())
+        
+else:
+    print("Failed to retrieve")
+    
