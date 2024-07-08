156864f419f6bc08e88051a25d05c9b0f260a716
William <willspri@gmail.com>
2024-07-08 19:00:39 +1200

    url fixed

--- repoScrape.py
+++ repoScrape.py
@@ -1,6 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
 
 repo = input("enter github profile name: ")
 
-URL = "github.com/" + repo + "?tab=repositories"
+URL = "https://github.com/" + repo + "?tab=repositories"
 
 print(URL)
+
+path_to_scrape = requests.get(URL)
+print(path_to_scrape)
